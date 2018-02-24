ae014e1069cabf3235e6c618041e5376e235021b
Daniel Shikoff <danielshikoff@Daniels-MacBook-Pro.local>
2018-02-23 19:12:39 -0800

    friday work

---
+++ index.js
@@ -0,0 +1,28 @@
+// var navLink = $('.link')
+$('.link1').mousedown(function() {
+  $(this).toggleClass('clickedLink1')
+})
+$('.link1').mouseup(function() {
+  $(this).toggleClass('clickedLink1')
+})
+
+$('.link2').mousedown(function() {
+  $(this).toggleClass('clickedLink2')
+})
+$('.link2').mouseup(function() {
+  $(this).toggleClass('clickedLink2')
+})
+
+$('.link3').mousedown(function() {
+  $(this).toggleClass('clickedLink3')
+})
+$('.link3').mouseup(function() {
+  $(this).toggleClass('clickedLink3')
+})
+
+$('.link4').mousedown(function() {
+  $(this).toggleClass('clickedLink4')
+})
+$('.link4').mouseup(function() {
+  $(this).toggleClass('clickedLink4')
+})
